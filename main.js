c70f5921e0b81063272862009db6c34a5b7588bd
AAWA-byt <mail@aaron.is>
2022-04-12 00:14:21 +0200

    :zap: changed log message

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
-    console.log('[Discord] Bot is online');
+    console.log('[Discord] Bot online');
     console.log(new Date())
 });
 
