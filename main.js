cc85e21b2a4bcf7381d6f21585cefe9e712e8c99
AAWA-byt <mail@aaron.is>
2022-04-12 00:14:03 +0200

    :tada: First Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
+
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log('[Discord] Bot is online');
+    console.log(new Date())
+});
+
+client.login(config.token);
